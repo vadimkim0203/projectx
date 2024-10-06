e25c11b812dcb0f612e21731594926525221219a
Vadim Kim <vkim0203@gmail.com>
2024-10-06 17:07:42 +0900

    Nav bar finished

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,9 +1,93 @@
 "use client"
 
+import Image from "next/image"
+
 const CartModal = () => {
+
+  const cartItems = true
+
   return (
-    <div className="">CartModal</div>
+    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+      {!cartItems ? (
+        <div className="">Cart is Empty</div>
+      ):
+      (
+        <>
+        <h2 className="text-xl">Shopping cart</h2>
+      {/* LIST */}
+      <div className="flex flex-col gap-8">
+        {/* ITEM */}
+        <div className="flex gap-4">
+          <Image src="https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&w=800" 
+          alt="" 
+          width={72} 
+          height={96} 
+          className="object-cover rounded-md"/>
+          <div className=" flex flex-col justify-between w-full">
+            {/* TOP */}
+              <div className="">
+                {/* TITLE */}
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Sapogi Skorohody</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$500</div>
+                  </div>
+                {/* DESCRIPTION */}
+                <div className="text-sm text-gray-500">description</div>
+              </div>
+
+            {/* BOTTOM */}
+            <div className="flex justify-between text-sm">
+              <span className="text-gray-500">Quantity 2</span>
+              <span className="text-blue-500">Remove</span>
+
+            </div>
+
+          </div>
+        </div>
+        {/* ITEM */}
+        <div className="flex gap-4">
+          <Image src="https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&w=800" 
+          alt="" 
+          width={72} 
+          height={96} 
+          className="object-cover rounded-md"/>
+          <div className=" flex flex-col justify-between w-full">
+            {/* TOP */}
+              <div className="">
+                {/* TITLE */}
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Shoostrie Pedali</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$325</div>
+                  </div>
+                {/* DESCRIPTION */}
+                <div className="text-sm text-gray-500">description</div>
+              </div>
+
+            {/* BOTTOM */}
+            <div className="flex justify-between text-sm">
+              <span className="text-gray-500">Quantity 2</span>
+              <span className="text-blue-500">Remove</span>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+      {/* BOTTOM */}
+      <div className="">
+        <div className="flex items-center justify-between font-semibold">
+          <span className="">Subtotal</span>
+          <span className="">Amount</span>
+        </div>
+        <p className="text-gray-500 text-sm mt-2 mb-4">Shipping and taxes calculated at checkout</p>
+        <div className="flex justify-between text-sm">
+          <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">View cart</button>
+          <button className="rounded-md py-3 px-4 bg-black text-white">Checkout</button>
+        </div>
+      </div>
+      </>
+    )}
+    </div>
   )
 }
-
 export default CartModal 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,15 +20,22 @@ const Navbar = () => {
       {/* BIGGER SCREENS */}
       <div className='hidden md:flex items-center justify-between gap-8 h-full'>
         {/* LEFT */}
-        <div className='w-1/3'>
+        <div className='w-1/3 xl:w-1/2 flex items-center gap-12'>
           <Link href="/" className="flex items-center gap-3">
             <Image src="/icons/shoppingCart.png" alt="" width={24} height={24}/>
             <div className='text-2xl tracking-wide'>PROJECTX
             </div>
           </Link>
+          <div className='hidden xl:flex gap-4'>
+            <Link href="/">Homepage</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
         </div>
         {/* RIGHT */}
-        <div className="w-2/3 flex items-center justify-between gap-8">
+        <div className="w-2/3 flex items-center justify-between gap-8 xl:w-1/2">
           <SearchBar/>
           <NavIcons/>
         </div>
