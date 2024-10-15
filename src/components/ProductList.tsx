23906c98d5cfe92ac3e4e232ca0f881671508d46
Vadim Kim <vkim0203@gmail.com>
2024-10-15 18:47:32 +0900

    Footer added

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const ProductList = () => {
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:[22%]">
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
         <div className="relative w-full h-80">
           <Image 
           src="https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
@@ -24,7 +24,73 @@ const ProductList = () => {
           <span className="font-semibold">129$</span>
         </div>
         <div className="text-sm text-gray-500">My description</div>
-        <button className="rounded-2xl ring-1 ring-lama text-black py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
+        <button className="rounded-2xl ring-1 ring-black bg-black text-white w-max py-2 px-4 text-xs hover:bg-white hover:text-black">Add to Cart</button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="relative w-full h-80">
+          <Image 
+          src="https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+          alt="" 
+          fill 
+          sizes="25vw" 
+          className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500"
+          />
+          <Image 
+          src="https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=800" 
+          alt="" 
+          fill sizes="25vw"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">129$</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-black bg-black text-white w-max py-2 px-4 text-xs hover:bg-white hover:text-black">Add to Cart</button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="relative w-full h-80">
+          <Image 
+          src="https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+          alt="" 
+          fill 
+          sizes="25vw" 
+          className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500"
+          />
+          <Image 
+          src="https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=800" 
+          alt="" 
+          fill sizes="25vw"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">129$</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-black bg-black text-white w-max py-2 px-4 text-xs hover:bg-white hover:text-black">Add to Cart</button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="relative w-full h-80">
+          <Image 
+          src="https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+          alt="" 
+          fill 
+          sizes="25vw" 
+          className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500"
+          />
+          <Image 
+          src="https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=800" 
+          alt="" 
+          fill sizes="25vw"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">129$</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-black bg-black text-white w-max py-2 px-4 text-xs hover:bg-white hover:text-black">Add to Cart</button>
       </Link>
     </div>
   );
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,72 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div className=''>Footer</div>
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-gray-100 text-sm mt-24">
+      {/* TOP */}
+      <div className="flex flex-col md:flex-row justify-between gap-24">
+        {/* LEFT */}
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <Link href="/">
+            <div className='text-2xl tracking-wide'>TROVE</div>
+          </Link>
+          <p className="">Seoul, South Korea</p>
+          <span className="font-semibold">oceanstorecs@gmail.com</span>
+          <span className="font-semibold">+82 10 43750585</span>
+          <div className="flex gap-6">
+            <Image src="/icons/instagram.png" alt="" width={20} height={20}/>
+            <Image src="/icons/facebook.png" alt="" width={20} height={20}/>
+            <Image src="/icons/youtube.png" alt="" width={20} height={20}/>
+            <Image src="/icons/x.png" alt="" width={20} height={20}/>
+          </div>
+        </div>
+        {/* CENTER */}
+        <div className="w-1/2 hidden lg:flex justify-between">
+          <div className="flex flex-col gap-6">
+            <h1 className="font-md text-lg">COMPANY</h1>
+            <div className="flex flex-col justify-between">
+              <Link href="">About Us</Link>
+              <Link href="">Contact Us</Link>
+            </div>
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <h1 className="font-medium text-lg">SUBSCRIBE</h1>
+          <p className="">Be the first to get the latest news about trends, promotions and much more!</p>
+          <div className="flex">
+            <input 
+            type="text" 
+            placeholder="Email address" 
+            className="p-4 w-3/4 " 
+            />
+            <button className="w-1/4 bg-black text-white">JOIN</button>
+          </div>
+            <span className="font-semibold">Secure Payments</span>
+              <div className="flex justify-between">
+                <Image src="/icons/visa.png" alt="" width={40} height={20}/>
+                <Image src="/icons/mastercard.png" alt="" width={40} height={20}/>
+                <Image src="/icons/paypal.png" alt="" width={40} height={20}/>
+              </div>
+          </div>
+        </div>
+      
+      {/* BOTTOM */}
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
+        <div className="">2024 Trove Store</div>
+        <div className="flex flex-col gap-8 md:flex-row">
+          <div className="">
+            <span className="text-gray-500 mr-4">Language</span>
+            <span className="font-medium">Korea | English</span>
+          </div>
+          <div className="">
+            <span className="text-gray-500 mr-4">Currency</span>
+            <span className="font-medium">$ USD</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 };
 
