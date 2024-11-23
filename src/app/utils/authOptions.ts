b415db7a4e24f4e2b38a462739e9fbc070eed45c
Azat Azamat <azat.kariuly@gmail.com>
2024-11-23 13:22:10 +0900

    update: google auth

--- src/app/utils/authOptions.ts
+++ src/app/utils/authOptions.ts
@@ -38,8 +38,8 @@ export const authOptions : AuthOptions = {
             }
         }),
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
         })
     ],
     callbacks: {
